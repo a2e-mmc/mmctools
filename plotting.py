68392f5dd9df84782d8508fd6d59ce80e6683d1b
Eliot Quon <eliot.quon@nrel.gov>
2019-02-22 14:44:22 -0700

    Add stubs for new code

---
+++ plotting.py
@@ -0,0 +1,13 @@
+"""
+Library of standardized plotting functions for basic plot formats
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_timeheight():
+    """Make 2D time-height contour plot"""
+    print('do something')
+
+def plot_timehistory_at_height():
+    print('do something')
+
