18e1da929b379695a5b140975c0d5cdc80784d89
Eliot Quon <eliot.quon@nrel.gov>
2019-03-15 14:39:39 -0600

    Add dataloaders.read_dir()

--- dataloaders.py
+++ dataloaders.py
@@ -4,10 +4,57 @@ Data loading routines
 
 Based on https://github.com/NWTC/datatools/blob/master/wfip2.py
 """
+import os,glob
 import numpy as np
 import pandas as pd
 
+
 reader_exceptions = (IOError, UnicodeDecodeError, AssertionError, ValueError)
+
+
+def read_dir(dpath='.',
+             reader=pd.read_csv,
+             file_filter='*',
+             ext='csv',
+             sort=True,
+             verbose=False,
+             **kwargs):
+    """Wrapper around pandas read_csv() or data reader function. 
+    
+    Additional readers:
+    - metmast
+    - measurements/radar
+    - measurements/lidar
+    - measurements/sodar
+    
+    Returns concatenated dataframe made up of dataframes read from CSV
+    files in specified directory. Filenames may be filtered with the 
+    file_filter argument, which is used to select files with globbing.
+
+    Additional keyword arguments are passed to the data reader.
+    """
+    dataframes = []
+    fpathlist = glob.glob(os.path.join(dpath,file_filter))
+    if sort:
+        fpathlist.sort()
+    for fpath in fpathlist:
+        if not os.path.isfile(fpath): continue
+        if not fpath.endswith(ext):
+            continue
+        if verbose:
+            print('Reading '+fpath)
+        try:
+            df = reader(fpath,**kwargs)
+        except reader_exceptions as err:
+            print(err,'while reading',fpath)
+        dataframes.append(df)
+    if len(dataframes) == 0:
+        print('No dataframes were read!')
+        df = None
+    else:
+        df = pd.concat(dataframes)
+    return df
+
 
 def read_date_dirs(dpath='.',
                    expected_date_format=None,
