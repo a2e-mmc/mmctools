055c9055b3fff6f3e78fcea97e7b6e84e3ff205d
Eliot Quon <eliot.quon@nrel.gov>
2019-03-19 00:40:25 -0600

    Add a few alternative Tv calculations
    For sanity checks, based on formulas (including one based on dewpoint
    temperature) at https://www.weather.gov/epz/wxcalc

--- helper_functions.py
+++ helper_functions.py
@@ -13,13 +13,17 @@ def T_to_Tv(T,p=None,RH=None,w=None,e=None,pd=None,epsilon=0.622,verbose=False):
     constants of dry air to water vapor.
     """
     if (p is not None) and (RH is not None):
-        # saturation vapor pressure of water, es [mbar]
+        # saturation vapor pressure of water, e_s [mbar]
         #   Eqn 10 from Bolton (1980), Mon. Weather Rev., Vol 108
         T_degC = T - 273.15
         es = 6.112 * np.exp(17.67*T_degC / (T_degC + 243.5))
         if verbose:
-            print('e_s(T) =',es)
+            # from https://www.weather.gov/media/epz/wxcalc/vaporPressure.pdf
+            # - is it 237.3 or 237.7?
+            es_est = 6.11 * 10**(7.5*T_degC/(237.3+T_degC))
+            print('e_s(T) =',es,'est',es_est)
         # saturation mixing ratio, ws [-]
+        #   Wallace & Hobbs, Eqn 3.63
         ws = epsilon * es / (p - es)
         if verbose:
             print('w_s(T,p) =',ws,'est',epsilon*es/p)
@@ -29,6 +33,19 @@ def T_to_Tv(T,p=None,RH=None,w=None,e=None,pd=None,epsilon=0.622,verbose=False):
         if verbose:
             q = w / (1+w)
             print('q(T,p,RH) =',q)
+        # alternative calculation, based on dewpoint
+        if verbose:
+            # from https://www.weather.gov/media/epz/wxcalc/virtualTemperature.pdf
+            # - note this is Wallace & Hobbs' eqn 3.16, also implemented  below
+            # - note the expression for vapor pressure is similar to the
+            #   saturation vapor pressure expression above, with Td instead of T
+            # - is it 237.3 or 237.7?
+            Td_degC = 237.3 * np.log(es*RH/611) / (7.5*np.log(10) - np.log(es*RH/611))
+            print('Td(T,RH) =',Td_degC)
+            e_est = 6.11 * 10**(7.5*Td_degC/(237.7+Td_degC))
+            denom = 1 - 0.379 * e_est/p
+            Tv_est = T / denom
+            print('est Tv(T,p,RH) =',Tv_est)
         # Using Wallace & Hobbs, Eqn 3.59
         if verbose:
             print('Tv(T,p,RH) ~=',T*(1+0.61*w))
