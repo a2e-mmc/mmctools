ae038c82b8457f982e1ea595fa6f727c8cb6c297
Eliot Quon <eliot.quon@nrel.gov>
2019-04-25 17:28:08 -0600

    Add power-law functions from NWTC/abl/meteorology

--- helper_functions.py
+++ helper_functions.py
@@ -148,3 +148,23 @@ def covariance(a,b,interval):
     ab_mean = (a*b).rolling(interval).mean()
     return ab_mean - a_mean*b_mean
 
+
+def power_law(z,zref=80.0,Uref=8.0,alpha=0.2):
+    return Uref*(z/zref)**alpha
+
+def fit_power_law_alpha(z,U,zref=80.0,Uref=8.0):
+    from scipy.optimize import curve_fit
+    above0 = (z > 0)
+    logz = np.log(z[above0]) - np.log(zref)
+    logU = np.log(U[above0]) - np.log(Uref)
+    fun = lambda xdata,alpha: alpha*xdata
+    popt, pcov = curve_fit(fun, xdata=logz, ydata=logU,
+                           p0=0.2, bounds=(0,np.inf))
+    alpha = popt[0]
+    resid = U - Uref*(z/zref)**alpha
+    SSres = np.sum(resid**2)
+    SStot = np.sum((U - np.mean(U))**2)
+    R2 = 1.0 - (SSres/SStot)
+    return alpha, R2
+
+
