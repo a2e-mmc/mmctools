0d2b86b28766fb4e251144c5f12a4131850768c1
Dries Allaerts <dries.allaerts@nrel.gov>
2019-05-30 14:14:04 -0600

    Implement reader to extract column from wrf data

--- wrf/utils.py
+++ wrf/utils.py
@@ -19,7 +19,17 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import netCDF4
 import xarray
+import utm # need to pip install!
+from scipy.spatial import KDTree
+from scipy.interpolate import interp1d, LinearNDInterpolator
 
+
+# List of default WRF fields for extract_column_from_wrfdata
+default_3D_fields = ['U10','V10','T2','TSK','UST','PSFC','HFX','LH','MUU','MUV','MUT']
+default_4D_fields = ['U','V','W','T',
+                     'RU_TEND','RU_TEND_ADV','RU_TEND_PGF','RU_TEND_COR','RU_TEND_PHYS',
+                     'RV_TEND','RV_TEND_ADV','RV_TEND_PGF','RV_TEND_COR','RV_TEND_PHYS',
+                     'T_TEND_ADV',]
 
 def _get_dim(wrfdata,dimname):
     """Returns the specified dimension, with support for both netCDF4
@@ -34,6 +44,25 @@ def _get_dim(wrfdata,dimname):
     elif isinstance(wrfdata, xarray.Dataset):
         try:
             return wrfdata.dims[dimname]
+        except KeyError:
+            print('No {:s} dimension'.format(dimname))
+            return None
+    else:
+        raise AttributeError('Unexpected WRF data type')
+
+def _get_dim_names(wrfdata,varname):
+    """Returns dimension names of the specified variable,
+    with support for both netCDF4 and xarray
+    """
+    if isinstance(wrfdata, netCDF4.Dataset):
+        try:
+            return wrfdata.variables[varname].dimensions
+        except KeyError:
+            print('No {:s} dimension'.format(dimname))
+            return None
+    elif isinstance(wrfdata, xarray.Dataset):
+        try:
+            return wrfdata.variables[varname].dims
         except KeyError:
             print('No {:s} dimension'.format(dimname))
             return None
@@ -105,6 +134,20 @@ def get_height_at_ind(wrfdata,j,i):
             zs[tt,:] = ((ph+phb)/9.81) - hgt
         z  = (zs[:,1:] + zs[:,:-1])*0.5
     return z,zs
+
+def get_unstaggered_var(wrfdata,varname):
+    '''
+    Extracts and unstaggers the specified variable
+    '''
+    var = _get_var(wrfdata,varname)
+    # Use dimension name to determine stagerred axis (staggered dimension name contain 'stag')
+    stag_axs = [dim.endswith('_stag') for dim in _get_dim_names(wrfdata,varname)]
+    assert(stag_axs.count(True) in [0,1]), \
+        'Multiple staggered axis not supported (field ='+field+')'
+    try:
+        return unstagger(var,stag_axs.index(True))
+    except ValueError:
+        return var
 
 def get_wrf_files(dpath='.',prefix='wrfout',returnFileNames=True,
                   fullpath=False,sort=True):
@@ -282,3 +325,226 @@ def unstagger(var,axis):
     # right_indx: (:,:,etc.) and replace ":" at ax with "1:"
     right_indx = [slice(1,None) if axi==axis else slice(None) for axi in range(var.ndim)]
     return (var[tuple(left_indx)] + var[tuple(right_indx)])/2.0
+
+def add_surface_plane(var,plane=None):
+    tdim,zdim,ydim,xdim = var.shape
+    if plane is None:
+        plane = np.zeros((tdim,1,ydim,xdim))
+        return np.concatenate((plane,var), axis = 1)
+    else:
+        return np.concatenate((np.reshape(plane,(tdim,1,ydim,xdim)),var), axis = 1)
+
+def extract_column_from_wrfdata(fpath, coords,
+                                Ztop=2000., Vres=5.0,
+                                T0=300.,
+                                spatial_filter='interpolate',L_filter=0.0,
+                                additional_fields=[],
+                                verbose=False,
+                               ):
+    '''
+    Extract a column of time-height data from a 4-dimensional WRF output file
+    '''
+    
+    # Load WRF data
+    xa = xarray.open_dataset(fpath)
+    tdim, zdim, ydim, xdim = get_wrf_dims(xa)
+    
+    
+    #---------------------------
+    # Preprocessing
+    #---------------------------
+    
+    # Extract WRF grid resolution
+    dx_meso = xa.attrs['DX']
+    assert(dx_meso == xa.attrs['DY'])
+    
+    
+    # Number of additional points besides nearest grid point to perform spatial filtering
+    if spatial_filter == 'interpolate':
+        Nadd = 1
+    elif spatial_filter == 'average':
+        Nadd = int(np.ceil(0.5*L_filter/dx_meso+1.0e-6)) # +eps to make sure Nadd*dxmeso > L_filter/2
+    else:
+        Nadd = 0
+        
+    
+    # Setup microscale grid data
+    site_X, site_Y, site_zonenumber, _ = utm.from_latlon(coords[0],coords[1])
+
+    if spatial_filter == 'average':
+        Navg = 6 #Number of interpolation points to compute average
+        xmicro = np.linspace(-L_filter/2,L_filter/2,Navg,endpoint=True)+site_X
+        ymicro = np.linspace(-L_filter/2,L_filter/2,Navg,endpoint=True)+site_Y
+    else: # 'interpolate' or 'nearest'
+        xmicro = site_X
+        ymicro = site_Y
+    
+    zmicro = np.linspace(0,Ztop,1+int(Ztop/Vres))
+    Zmicro, Ymicro, Xmicro = np.meshgrid(zmicro, ymicro, xmicro, indexing='ij')
+
+    #2D and 3D list of points
+    XYmicro = np.array((Ymicro[0,:,:].ravel(),Xmicro[0,:,:].ravel())).T
+    XYZmicro = np.array((Zmicro.ravel(),Ymicro.ravel(),Xmicro.ravel())).T
+    
+
+    # Check whether additional fields are 3D or 4D and append to corresnponding list of fields
+    fieldnames_3D = default_3D_fields
+    fieldnames_4D = default_4D_fields
+    for field in additional_fields:
+        try:
+            ndim = len(xa[field].dims)
+        except KeyError:
+            print('The additional field "'+field+'" is not available and will be ignored.')
+        else:
+            if len(xa[field].dims) == 3:
+                fieldnames_3D.append(field)
+            elif len(xa[field].dims) == 4:
+                fieldnames_4D.append(field)
+            else:
+                raise Exception('Field "'+field+'" is not 3D or 4D, not sure how to process this field.')
+    
+    
+    #---------------------------
+    # Load data
+    #---------------------------
+    
+    WRFdata = {}
+    
+    # Cell-centered coordinates
+    XLAT = xa.variables['XLAT'].values     # WRF indexing XLAT[time,lat,lon]
+    XLONG = xa.variables['XLONG'].values
+    
+    # Height above ground level
+    WRFdata['Zagl'],_ = get_height(xa,timevarying=True)
+    WRFdata['Zagl']   = add_surface_plane(WRFdata['Zagl'])
+    
+    # 3D fields. WRF indexing Z[tdim,ydim,xdim]
+    for field in fieldnames_3D:
+        WRFdata[field] = get_unstaggered_var(xa,field)
+        
+    
+    # 4D fields. WRF indexing Z[tdim,zdim,ydim,xdim]
+    for field in fieldnames_4D:
+        WRFdata[field] = get_unstaggered_var(xa,field)
+            
+        # 4D field specific processing
+        if field is 'T':
+            # Add T0, set surface plane to TSK
+            WRFdata[field] += T0
+            WRFdata[field] = add_surface_plane(WRFdata[field],plane=WRFdata['TSK'])
+        elif field in ['U','V','W']:
+            # Set surface plane to zero (no slip)
+            WRFdata[field] = add_surface_plane(WRFdata[field])
+        else:
+            WRFdata[field] = add_surface_plane(WRFdata[field],plane=WRFdata[field][:,0,:,:])
+    
+    
+    #---------------------------
+    # Extract site data
+    #---------------------------
+    sitedata = {}
+    sitedata['Zagl'] = zmicro
+    
+    # Nearest grid points to the site
+    points = np.array((XLAT[0,:,:].ravel(),XLONG[0,:,:].ravel())).T
+    tree = KDTree(points)
+    dist, index = tree.query(np.array(coords),1) # nearest grid point
+    inear = int( index % xdim )       # index to XLONG
+    jnear = int((index-inear) / xdim) # index to XLAT
+    
+    
+    # Extract data and apply spatial filter if necessary
+    if spatial_filter == 'nearest':
+        # - 3D fields
+        for field in fieldnames_3D:
+            sitedata[field] = WRFdata[field][:,jnear,inear]
+               
+        # - 4D fields
+        for field in fieldnames_4D:
+            sitedata[field] = np.zeros((tdim,zmicro.size))
+        
+        # Interpolate to microscale z grid at every time
+        for t in range(tdim):
+            Zmeso = WRFdata['Zagl'][t,:,jnear,inear].squeeze()
+            wrf_data_combined  = np.array([WRFdata[field][t,:,jnear,inear].squeeze() for field in fieldnames_4D]).T
+            site_data_combined = interp1d(Zmeso,wrf_data_combined,axis=0)(zmicro)
+            for l, field in enumerate(fieldnames_4D):
+                sitedata[field][t,:] = site_data_combined[:,l]
+            
+            
+    else: # 'interpolate' or 'average'
+        # Coordinates of a subset of the WRF grid in UTM-projected cartesian system
+        NN = 1+2*Nadd
+        Xmeso = np.zeros((NN,NN))
+        Ymeso = np.zeros((NN,NN))
+        for i,ii in enumerate(range(inear-Nadd,inear+Nadd+1)):
+            for j,jj in enumerate(range(jnear-Nadd,jnear+Nadd+1)):
+                Xmeso[j,i], Ymeso[j,i], _, _ = utm.from_latlon(XLAT[0,jj,ii],
+                                                               XLONG[0,jj,ii],
+                                                               force_zone_number = site_zonenumber)
+                
+        Xmeso = np.repeat(Xmeso[np.newaxis, :, :], zdim+1, axis=0)
+        Ymeso = np.repeat(Ymeso[np.newaxis, :, :], zdim+1, axis=0)
+        XYmeso = np.array((np.ravel(Ymeso[0,:,:]),np.ravel(Xmeso[0,:,:]))).T
+        
+        
+        #Initialize fields
+        for field in fieldnames_3D: sitedata[field] = np.zeros((tdim))
+        for field in fieldnames_4D: sitedata[field] = np.zeros((tdim,zmicro.size))
+            
+        #Perform 3D interpolation to microscale grid for every time
+        for t in range(tdim):
+            # 3D fields
+            slice3d = (t,slice(jnear-Nadd,jnear+Nadd+1),slice(inear-Nadd,inear+Nadd+1))
+            wrf_data_combined  = np.array([WRFdata[field][slice3d].ravel() for field in fieldnames_3D]).T
+            site_data_combined = LinearNDInterpolator(XYmeso,wrf_data_combined)(XYmicro)
+            for l, field in enumerate(fieldnames_3D):
+                if spatial_filter == 'interpolate':
+                    sitedata[field][t] = site_data_combined[0,l]
+                elif spatial_filter == 'average':
+                    sitedata[field][t] = np.mean(site_data_combined[:,l])
+            
+            
+            # 4D fields
+            slice4d = (t,range(zdim+1),slice(jnear-Nadd,jnear+Nadd+1),slice(inear-Nadd,inear+Nadd+1))
+            Zmeso = WRFdata['Zagl'][slice4d]
+            XYZmeso = np.array((Zmeso.ravel(),Ymeso.ravel(),Xmeso.ravel())).T
+            
+            wrf_data_combined  = np.array([WRFdata[field][slice4d].ravel() for field in fieldnames_4D]).T
+            site_data_combined = LinearNDInterpolator(XYZmeso,wrf_data_combined)(XYZmicro)
+            for l, field in enumerate(fieldnames_4D):
+                if spatial_filter == 'interpolate':
+                    sitedata[field][t,:] = site_data_combined[:,l]
+                elif spatial_filter == 'average':
+                    sitedata[field][t,:] = np.mean(site_data_combined[:,l].reshape(Zmicro.shape), axis=(1,2))
+
+                    
+    #---------------------------
+    # Store data in xarray
+    #---------------------------
+    
+    coords = {'Time': xa['XTIME'].values,
+              'height': zmicro}
+    
+    data_vars = {}
+    for field in fieldnames_3D:
+        data_vars[field] = ('Time',
+                            sitedata[field],
+                            {'description': xa[field].attrs['description'],
+                             'units': xa[field].attrs['units']}
+                           )
+    for field in fieldnames_4D:
+        data_vars[field] = (['Time','height'],
+                            sitedata[field],
+                            {'description': xa[field].attrs['description'],
+                             'units': xa[field].attrs['units']}
+                           )
+        
+    
+    xn = xarray.Dataset(data_vars=data_vars,coords=coords)
+    
+    # Rename T to theta and adjust description
+    xn = xn.rename({'T':'theta'})
+    xn['theta'].attrs['description'] = 'potential temperature'
+
+    return xn
