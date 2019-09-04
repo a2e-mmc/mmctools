b6bbd4e5b3632959db248fed55a5fbdb3e6c4723
Eliot Quon <eliot.quon@nrel.gov>
2019-09-04 13:03:00 -0600

    Add time info to TowerArray object, implement data processing
    Note: tslist is now saved with the prefix (corresponding to the sampling
    file name) as the dataframe index

--- wrf/ts.py
+++ wrf/ts.py
@@ -21,7 +21,10 @@ class TowerArray(object):
     """
     varnames = ['uu','vv','ww','th','pr','ph']
 
-    def __init__(self,outdir,casedir,domain,towersubdir='towers',verbose=True):
+    def __init__(self,outdir,casedir,domain,
+                 starttime,timestep=10.0,
+                 towersubdir='towers',
+                 verbose=True):
         """Create a TowerArray object from a WRF simulation with tslist
         output
 
@@ -33,6 +36,13 @@ class TowerArray(object):
         casedir : str
             Directory path to where WRF was run, which should contain
             the tslist file and a towers subdirectory.
+        starttime : str or Timestamp
+            The datetime at which the simulation was started
+            (corresponding to t=0 in the sampling output), which should
+            correspond to the start_* namelist parameters in the WRF
+            namelist.input.
+        timestep : float
+            The timestep for the selected WRF domain, in seconds.
         domain : int
             WRF domain to use (domain >= 1)
         towersubdir : str, optional
@@ -41,6 +51,8 @@ class TowerArray(object):
         self.verbose = verbose # for debugging
         self.outdir = outdir
         self.casedir = casedir
+        self.starttime = pd.to_datetime(starttime)
+        self.timestep = timestep
         self.domain = domain
         self.tslistpath = os.path.join(casedir,'tslist')
         self.towerdir = os.path.join(casedir, towersubdir)
@@ -64,12 +76,13 @@ class TowerArray(object):
                                                                  self.domain,
                                                                  varname.upper()))
                 assert os.path.isfile(fpath), '{:s} not found'.format(fpath)
+        self.tslist.set_index('prefix',inplace=True)
 
     def _load_data(self):
         """Load ncfile(s) if they exist, or generate them using the
         Tower class"""
         self.data = {}
-        for prefix in self.tslist['prefix']:
+        for prefix in self.tslist.index:
             fpath = os.path.join(self.outdir, prefix+'.nc')
             if os.path.isfile(fpath):
                 if self.verbose: print('Reading',fpath)
@@ -83,12 +96,13 @@ class TowerArray(object):
         add tower latitude/longitude. Setting them as indices makes the
         recognizable as coordinates by xarray.
         """
-        fpath = os.path.join(self.towerdir,'{:s}.d{:02d}.*'.format(prefix,self.domain))
-        df = Tower(fpath,varlist=self.varnames).to_dataframe(start_time=starttime,
-                                                             time_step=dt,
-                                                             heights=None)
-        df['lat'] = self.tslist[prefix]['lat']
-        df['lon'] = self.tslist[prefix]['lon']
+        towerfile = '{:s}.d{:02d}.*'.format(prefix, self.domain)
+        fpath = os.path.join(self.towerdir,towerfile)
+        df = Tower(fpath,varlist=self.varnames).to_dataframe(
+                start_time=self.starttime, time_step=self.timestep, heights=None)
+        towerinfo = self.tslist.loc[prefix]
+        df['lat'] = towerinfo['lat']
+        df['lon'] = towerinfo['lon']
         df.set_index(['lat','lon'], append=True, inplace=True)
         nc = df.to_xarray()
         if outfile is not None:
