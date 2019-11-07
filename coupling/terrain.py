55139364afb7cd73163b06022c46bfebca1077ab
Eliot Quon <eliot.quon@nrel.gov>
2019-11-07 05:37:53 -0700

    Implement margin kwarg in __init__
    instead of in download, for consistency between bounds of the clipped
    data and the bounds used for transformation

--- coupling/terrain.py
+++ coupling/terrain.py
@@ -26,7 +26,8 @@ class SRTM(object):
     """Class for working with Shuttle Radar Topography Mission data"""
     data_products = ['SRTM1','SRTM3']
 
-    def __init__(self,latlon_bounds,fpath='output.tif',product='SRTM3'):
+    def __init__(self,latlon_bounds,fpath='output.tif',product='SRTM3',
+                 margin=0.05):
         """Create container for SRTM data in the specified reegion
 
         Usage
@@ -39,24 +40,23 @@ class SRTM(object):
         product : str, optional
             Data product name, SRTM1 or SRTM3 (corresponding to 30- and
             90-m DEM).
+        margin : float, optional
+            Decimal degree margin added to the bounds (default is 3").
         """
-        self.bounds = latlon_bounds
+        self.bounds = list(latlon_bounds)
+        if margin is not None:
+            self.bounds[0] -= margin
+            self.bounds[1] -= margin
+            self.bounds[2] += margin
+            self.bounds[3] += margin
         self.output = fpath
         assert (product in self.data_products), \
                 'product should be one of '+str(self.data_products)
         self.product = product
 
-    def download(self,margin='0'):
-        """Download the SRTM data in GeoTIFF format
-
-        Usage
-        =====
-        margin : str, optional
-            Decimal degree margin added to the bounds. Use '%' for
-            percent margin.
-        """
-        elevation.clip(self.bounds, product=self.product, output=self.output,
-                       margin=margin)
+    def download(self):
+        """Download the SRTM data in GeoTIFF format"""
+        elevation.clip(self.bounds, product=self.product, output=self.output)
         elevation.clean()
 
     def to_terrain(self,dx,dy,resampling=warp.Resampling.bilinear):
