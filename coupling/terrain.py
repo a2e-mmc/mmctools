7e64e18053ee2133f0f68538547ff02687daa455
Eliot Quon <eliot.quon@nrel.gov>
2019-11-07 04:04:08 -0700

    Implement download()

--- coupling/terrain.py
+++ coupling/terrain.py
@@ -24,24 +24,36 @@ class SRTM(object):
     """Class for working with Shuttle Radar Topography Mission data"""
     data_products = ['SRTM1','SRTM3']
 
-    def __init__(self,latlon_bounds,output='output.tif',product='SRTM3'):
-        """Download SRTM data for the specified reegion
+    def __init__(self,latlon_bounds,fpath='output.tif',product='SRTM3'):
+        """Create container for SRTM data in the specified reegion
 
         Usage
         =====
         latlon_bounds : list or tuple
             Latitude/longitude corresponding to west, south, east, and
             north bounds.
-        output : str
+        fpath : str, optional
             Where to save downloaded GeoTIFF (*.tif) data.
-        product : str'
+        product : str, optional
             Data product name, SRTM1 or SRTM3 (corresponding to 30- and
             90-m DEM).
         """
         self.bounds = latlon_bounds
-        self.output = output
+        self.output = fpath
         assert (product in self.data_products), \
                 'product should be one of '+str(self.data_products)
         self.product = product
 
+    def download(self,margin='0'):
+        """Download the SRTM data in GeoTIFF format
+
+        Usage
+        =====
+        margin : str, optional
+            Decimal degree margin added to the bounds. Use '%' for
+            percent margin.
+        """
+        elevation.clip(self.bounds, product=self.product, output=self.output,
+                       margin=margin)
+        elevation.clean()
 
