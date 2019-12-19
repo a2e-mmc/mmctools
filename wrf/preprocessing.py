9edf9f11756fdab62c40687dbf70881873b22932
Eliot Quon <eliot.quon@nrel.gov>
2019-12-19 13:41:29 -0700

    Implement download() and ERAInterim RDADataset
    Inherited dataset classes should override the download() routine

--- wrf/preprocessing.py
+++ wrf/preprocessing.py
@@ -10,7 +10,8 @@ def prompt(s):
     else:
         return input(s)
 
-class Dataset(object):
+
+class RDADataset(object):
     """Class to help with downloading initial and boundary conditions
     to use with WPS.
     """
@@ -44,7 +45,7 @@ class Dataset(object):
         self.cookie = 'auth.rda.ucar.edu.' + str(pid)
         postdata = '--post-data="email={:s}&passwd={:s}&action=login"'.format(
                 self.emailaddr,self.passwd)
-        cmd = ['wget'] + self.certopts + self.opts
+        cmd = ['wget'] + self.certopts
         cmd += [
             '-O', 'auth_status.rda.ucar.edu',
             '--save-cookies', self.cookie,
@@ -62,4 +63,67 @@ class Dataset(object):
         if os.path.isfile(self.cookie):
             os.remove(self.cookie)
 
-    #def download(self):
+    def download(self,dataid,prefix,fields,datetimes):
+        """Download specified data at specified datetimes
+
+        Usage
+        =====
+        dataid : str
+            RDA dataset identifier, e.g. 'ds083.2' for FNL
+        prefix : str
+            Data prefix, e.g., 'ei.oper.an.pl'
+        fields : list of str
+            Field names, e.g., ['regn128sc','regn128uv']
+        datetimes : timestamp or list of timestamps
+            Datetime, e.g., output from
+            pd.date_range(startdate,enddate,freq='21600s')
+        """
+        url = 'https://rda.ucar.edu/data/{dataid:s}/{prefix:s}/{YY:d}{MM:02d}/'
+        url += '{prefix:s}.{field:s}.{YY:d}{MM:02d}{DD:02d}{HH:02d}'
+        cmd = ['wget'] + self.certopts + self.opts
+        cmd += [
+            '--load-cookies', self.cookie,
+            'URL_placeholder'
+        ]
+        if not hasattr(datetimes,'__iter__'):
+            datetimes = [pd.to_datetime(datetimes)]
+        print('Downloading fields',fields,'at',len(datetimes),'times')
+        for datetime in datetimes:
+            for field in fields:
+                cmd[-1] = url.format(dataid=dataid,
+                                     prefix=prefix,
+                                     field=field,
+                                     YY=datetime.year,
+                                     MM=datetime.month,
+                                     DD=datetime.day,
+                                     HH=datetime.hour)
+                p = subprocess.run(cmd)
+                p.check_returncode() 
+
+
+class ERAInterim(RDADataset):
+    """ERA-Interim Reanalysis
+
+    https://rda.ucar.edu/datasets/ds627.0/#!description
+    """
+
+    def download(self,datetimes):
+        """Download data at specified datetimes.
+
+        Usage
+        =====
+        datetimes : timestamp or list of timestamps
+            Datetime, e.g., output from
+            pd.date_range(startdate,enddate,freq='21600s')
+        """
+        # pressure-level data
+        super().download(dataid='ds627.0',
+                         prefix='ei.oper.an.pl',
+                         fields=['regn128sc','regn128uv'],
+                         datetimes=datetimes)
+        # surface data
+        super().download(dataid='ds627.0',
+                         prefix='ei.oper.an.sfc',
+                         fields=['regn128sc'],
+                         datetimes=datetimes)
+
