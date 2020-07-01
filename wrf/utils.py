dc567c7621ee6345427b34424d2d3ecc826df320
William Lassman <lassman1@llnl.gov>
2020-07-01 16:18:19 -0700

    Add memory error workaround to wrfout_seriesReader

--- wrf/utils.py
+++ wrf/utils.py
@@ -1029,6 +1029,7 @@ def combine_towers(fdir, restarts, simulation_start, fname,
         if verbose:
             print('restart: {}'.format(restart))
         data = []
+        print("FNAME: ",fname)
         for ff in fname:
             if verbose:
                 print('starting {}'.format(ff))
@@ -1041,20 +1042,29 @@ def combine_towers(fdir, restarts, simulation_start, fname,
                                height_var=height_var,
                                agl=agl,
                                **kwargs)
+            #print(ds)
             data.append(ds)
+            print("testing",ff,data)
         data_block = xr.combine_by_coords(data)
         if np.shape(restarts)[0] > 1:
+            
             if rst == 0:
+                print("RST == 0", rst )
                 data_previous = data_block
             else:
                 dataF = data_block.combine_first(data_previous)
                 data_previous = dataF
         else:
             dataF = data_block
+        print(np.shape(restarts)[0])
+        #if rst>0:
+        #    print(ds)
     if heights is None:
+        print("branch a, height is none")
         height_dim = 'k'
         height_var = 'ph'
     else:
+        print("branch b, height is NOT none")
         height_dim = 'height'
         height_var = 'height'
     if structure == 'ordered':
@@ -1071,6 +1081,9 @@ def combine_towers(fdir, restarts, simulation_start, fname,
         dataF.attrs['DX'] = dx
         dataF.attrs['DY'] = dy
     elif structure == 'unordered':
+        print("height_dim: ",height_dim)
+        print( dataF.dims )
+        print(dataF)
         dataF = dataF.rename_dims({height_dim:'nz'})
     dataF = dataF.assign_coords(lat=dataF.lat)
     dataF = dataF.assign_coords(lon=dataF.lon)
@@ -1126,14 +1139,14 @@ def tsout_seriesReader(fdir, restarts, simulation_start_time, domain_of_interest
             for twr_n in tower_names:
                 if twr in twr_n: good_towers.append(twr_n)
         tower_names = good_towers
-    
+    print( "testout_seriesReader stuff", fdir, tower_names )
     dsF = combine_towers(fdir,restarts,simulation_start_time,tower_names,
                          structure=structure, time_step=time_step,
                          heights=heights, height_var=height_var)
     return dsF
 
 
-def wrfout_seriesReader(wrf_path,wrf_file_filter,specified_heights=None):
+def wrfout_seriesReader(wrf_path,wrf_file_filter,specified_heights=None, hlim_ind = None):
     """
     Construct an a2e-mmc standard, xarrays-based, data structure from a
     series of 3-dimensional WRF output files
@@ -1152,6 +1165,15 @@ def wrfout_seriesReader(wrf_path,wrf_file_filter,specified_heights=None):
         If not None, then a list of static heights to which all data
         variables should be	interpolated. Note that this significantly
         increases the data read time.
+    hlim_ind : int, index
+        If not none, then the DataArray ds_subset is further subset by vertical dimension,
+        keeping vertical layers 0:hlim_ind.
+        This is meant to be used to speed up execution of the code or prevent a memory error
+        where the specified_heights argument is not well suited
+        (i.e., want a range of non-interpolated heights), and you only care about
+        data that are below a certain vertical index.
+        
+      
     """
     TH0 = 300.0 #WRF convention base-state theta = 300.0 K
     dims_dict = {
@@ -1230,7 +1252,16 @@ def wrfout_seriesReader(wrf_path,wrf_file_filter,specified_heights=None):
     #print(ds_subset)
     ds_subset = ds_subset.rename_dims(dims_dict)
     #print(ds_subset)
-    return ds_subset
+    
+    # Change by WHL to eliminate vertical info far from the surface to prevent memory crash                                       
+    try:
+        ds_subset2 = ds_subset.isel( nz = slice(0, hlim_ind) )
+        return ds_subset2
+    except:
+        # if hlim_ind = None, default code execution
+        return ds_subset
+
+    
 
 
 def write_tslist_file(fname,lat=None,lon=None,i=None,j=None,twr_names=None,twr_abbr=None):
