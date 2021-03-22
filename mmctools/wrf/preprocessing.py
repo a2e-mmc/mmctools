c68678b4883034e86b88ddcf5f4802d5c35b3195
Patrick Hawbecker <hawbecker.3@gmail.com>
2021-03-22 10:33:39 -0600

    Adding more options to the namelist

--- mmctools/wrf/preprocessing.py
+++ mmctools/wrf/preprocessing.py
@@ -512,6 +512,7 @@ class setup_wrf():
                          'relax_zone' : 4,
                 'nio_tasks_per_group' : 0,
                          'nio_groups' : 1,
+                         'sst_update' : 1,
             }
 
             namelist_opts = namelist_defaults
@@ -825,12 +826,18 @@ class setup_wrf():
         f.write(" num_soil_layers           = {}, \n".format(self.namelist_opts['num_soil_layers']))
         f.write(" num_land_cat              = {}, \n".format(self.namelist_opts['num_land_cat']))
         f.write(" sf_urban_physics          = {}\n".format(urb_str))
+        f.write(" sst_update                = {}, \n".format(self.namelist_opts['sst_update']))
         f.write(" /\n")
         f.write("\n")
         f.write("&fdda\n")
         f.write("/\n")
         f.write("\n")
         f.write("&dynamics\n")
+        if 'hybrid_opt' in self.namelist_opts:
+            f.write(" w_damping                 = {}, \n".format(self.namelist_opts['hybrid_opt']))
+        if 'use_theta_m' in self.namelist_opts:
+            f.write(" use_theta_m               = {}, \n".format(self.namelist_opts['use_theta_m']))
+
         f.write(" w_damping                 = {}, \n".format(self.namelist_opts['w_damping']))
         f.write(" diff_opt                  = {}\n".format(diff_str))
         f.write(" km_opt                    = {}\n".format(km_str))
@@ -990,6 +997,10 @@ class setup_wrf():
         add_str_start = '+:h:0:'
 
         for ii,io_name in enumerate(np.unique(io_names)):
+            if '"' in io_name:
+                io_name = io_name.replace('"','')
+            if "'" in io_name:
+                io_name = io_name.replace("'",'')
             f = open('{}{}'.format(self.run_dir,io_name),'w')
             line = ''
             if vars_to_remove is not None:
